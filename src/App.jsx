7b0d724e8bdd89cee3eed90f27bb687c859ef939
dver26 <fercorvictor@gmail.com>
2026-07-17 14:36:00 +0200

    Separacio Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import serveis from '../serveis.json'
 import './App.css'
 
+import Card from './Card'
+
 const App = () => {
   return (
     <div className='container-grid'>
@@ -9,36 +11,7 @@ const App = () => {
           (sum, part) => sum + part.duration,
           0
         )
-
-        return (
-          <div className='element-grid' key={servei.name}>
-            <div className='header'>
-              <h3 className='titulo'>{servei.name}</h3>
-              <span className='duracio-total'>{duracioTotal} min</span>
-            </div>
-
-            <ul className='parts-list'>
-              {servei.parts.map((part, i) => (
-                <li className='part' key={i}>
-                  <div className='part-info'>
-                    <span className='part-name'>{part.name}</span>
-                    <span className='part-duration'>{part.duration} min</span>
-                  </div>
-                  {part.availableEmployees &&
-                    part.availableEmployees.length > 0 && (
-                      <div className='empleats'>
-                        {part.availableEmployees.map((emp) => (
-                          <span className='badge' key={emp}>
-                            {emp}
-                          </span>
-                        ))}
-                      </div>
-                    )}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )
+        return <Card servei={servei} duracioTotal={duracioTotal} />
       })}
     </div>
   )
---
+++ src/Card.jsx
@@ -0,0 +1,37 @@
+import './Card.css'
+
+const Card = ({ servei, duracioTotal }) => {
+  return (
+    <>
+      <div className='element-grid' key={servei.name}>
+        <div className='header'>
+          <h3 className='titulo'>{servei.name}</h3>
+          <span className='duracio-total'>{duracioTotal} min</span>
+        </div>
+
+        <ul className='parts-list'>
+          {servei.parts.map((part, i) => (
+            <li className='part' key={i}>
+              <div className='part-info'>
+                <span className='part-name'>{part.name}</span>
+                <span className='part-duration'>{part.duration} min</span>
+              </div>
+              {part.availableEmployees &&
+                part.availableEmployees.length > 0 && (
+                  <div className='empleats'>
+                    {part.availableEmployees.map((emp) => (
+                      <span className='badge' key={emp}>
+                        {emp}
+                      </span>
+                    ))}
+                  </div>
+                )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Card
