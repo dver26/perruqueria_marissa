3266d09b828280ed75f6236fdd50294a6e3f5a8a
guipuca06-code <guipuca06@gmail.com>
2026-09-04 16:30:45 +0200

    Inici cliets

--- src/inicio/CardGrid.jsx
+++ src/inicio/CardGrid.jsx
@@ -4,14 +4,56 @@ import Card from './Card.jsx'
 
 import './CardGrid.css'
 
+import {fetchTabla} from '../utils/supabase.js'
+
+import { useEffect, useState } from 'react'
+
+
+
 const CardGrid = () => {
+
   const { state } = useAppContext()
-  return (
-    <div className='container-grid'>
-      {state.servicios.map((servei) => {
+  // setClients és el que t'actualitza el que hi ha a clients
+  const [ clients, setClients] = useState([])
+
+  useEffect(() => {
+
+    const Taula_clients = async () => {
+      const data = await fetchTabla('clientes')
+      console.log('Data de clientes:', data)
+      // Ara ja tenim les dades a clients, i podem utilitzar-les per renderitzar la taula o fer altres operacions
+      setClients(data)
+    }
+    Taula_clients()
+  }, [])
+
+  const handleClick = () => {
+    console.log('Botó Clients clicat')
+    // Aquí pots afegir la lògica per canviar de pantalla o fer altres accions quan es clica el botó
+  }
+
+  
+return (
+
+
+    <div > 
+      <div className = 'botons'> 
+        <button className='boto-clients' onClick={handleClick}>Clients
+
+        </button>
+
+      </div>
+      <div className='container-grid'>
+      {
+      state.servicios.map((servei) => {
         return <Card key={servei.id} id={servei.id} />
       })}
+     </div>
+      
     </div>
+
+    
+    
   )
 }
 
